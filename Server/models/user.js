f2c0af6047a07bf8f37139047c882481625b0394
TiagoRG99 <tiagoricog99@gmail.com>
2020-12-21 00:51:20 +0000

    Revert "Upload files"
    This reverts commit 95812bb42756e31291cae748c2421da30a4c0e32.

---
+++ Server/models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+var uniqueValidator = require('mongoose-unique-validator');
+
+var userSchema = new mongoose.Schema({
+    mail: { type: String, unique: true},
+    name: String,
+    activity: String,
+    course: String,
+    department: String,
+    role: String,
+    data_register: String,
+    data_last_login: String,
+    password_enc: String,
+});
+
+module.exports = mongoose.model('user', userSchema)
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,23 +1,23 @@
+
 var express = require('express');
 var router = express.Router();
 
-var passport = require('passport')
-
 // GET home page. 
-router.get('/', function (req, res) {
+router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/protegida', verificaAutenticacao, function (req, res) {
-  res.render('protegida', { utilizador: req.user.id });
+router.get('/protegida', verificaAutenticacao, function(req, res) {
+  res.render('protegida', {utilizador: req.user});
 });
 
-function verificaAutenticacao(req, res, next) {
+function verificaAutenticacao(req,res,next)
+{
   console.log('User (verif.: )' + JSON.stringify(req.user))
-  if (req.isAuthenticated()) {
+  if(req.isAuthenticated()){
     next();
   }
-  else {
+  else{
     res.redirect("/users/login");
   }
 }
