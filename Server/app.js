2c73dc6e448fb8780534b2f1c6b2a4e87018b8f1
TiagoRG99 <tiagoricog99@gmail.com>
2020-12-21 00:45:56 +0000

    Revert "merge"
    This reverts commit ed753a21bd3b534216551259a49a9ce8c4b3298b, reversing
    changes made to 0aad9a0138cdd72f24aeb320cea29261f56f14d5.

--- Server/app.js
+++ Server/app.js
@@ -3,7 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
+var User = require('../Server/controllers/user')
 
+ 
+//#region mongoose Configuration
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/DAW2020';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error...'));
+db.once('open', function() {
+    console.log("Conexão ao MongoDB realizada com sucesso...")
+});
+
+//#endregion
+
+//#region Autenticaçao ------------------------------------------------------
 var { v4: uuidv4 } = require('uuid');
 var session = require('express-session');
 const FileStore = require('session-file-store')(session);
@@ -12,33 +32,44 @@ var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy
 var axios = require('axios')
 
+
 // Configuração da estratégia local
 passport.use(new LocalStrategy(
-  { usernameField: 'id' }, function (username, password, done) {
-    axios.get('http://localhost:7711/users/' + username)
+  {usernameField: 'id'}, function(id, password, done) {
+    User.lookUp(id)
       .then(dados => {
-        const user = dados.data
-        if (!user) { return done(null, false, { message: 'Utilizador inexistente!\n' }) }
-        if (password != user.password) { return done(null, false, { message: 'Credenciais inválidas!\n' }) }
+        const user = dados
+        console.log(dados)
+        console.log(id)
+        console.log(password)
+        if(!user) { return done(null, false, {message: 'Utilizador inexistente!\n'})}
+        if(password != user.password_enc) { return done(null, false, {message: 'Credenciais inválidas!\n'})}
         return done(null, user)
       })
       .catch(erro => done(erro))
-  })
+    })
 )
 
-// Indica-se ao passport como serializar o utilizador
-passport.serializeUser((user, done) => {
-  console.log('Serielização, id: ' + user.id)
-  done(null, user.id)
-})
+//#endregion
 
+//#region passport
+// Indica-se ao passport como serializar o utilizador
+passport.serializeUser((user,done) => {
+  console.log('Serielização, id: ' + JSON.stringify(user))
+  done(null, user.mail)
+})
+  
 // Desserialização: a partir do id obtem-se a informação do utilizador
 passport.deserializeUser((uid, done) => {
   console.log('Desserielização, id: ' + uid)
-  axios.get('http://localhost:7711/users/' + uid)
-    .then(dados => done(null, dados.data))
+  User.lookUp(uid)
+    .then(dados => {
+      console.log(dados)
+      done(null, dados)})
     .catch(erro => done(erro, false))
 })
+
+//#endregion
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -46,19 +77,36 @@ const { RequestHeaderFieldsTooLarge } = require('http-errors');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//#region session
 app.use(session({
-  genid: function (req) {
+  genid: function(req) {
     return uuidv4()
   },
   store: new FileStore(),
   secret: 'DAW2020auth',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: true
 }))
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(function(req,res,next){
+  console.log(req.sessionID)
+  next()
+})
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next){
+  console.log('Signed Cookies: ', JSON.stringify(req.signedCookies))
+  console.log('Session: ', JSON.stringify(req.session))
+  next()
+})
+
+//#endregion
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -66,25 +114,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser('DAW2020auth'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(function (req, res, next) {
-  console.log('Signed Cookies: ', JSON.stringify(req.signedCookies))
-  console.log('Session: ', JSON.stringify(req.session))
-  next()
-})
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,138 +1,82 @@
 var express = require('express');
 var router = express.Router();
-var jsonfile = require('jsonfile')
-var fs = require('fs')
-var path = require('path')
+
+var User = require('../controllers/user')
 
 var passport = require('passport')
 
-var multer = require('multer')
-
-var upload = multer({ dest: 'uploads/' })
+//#region login e autorizaçao
 
 // GET users listing. 
-router.get('/login', function (req, res) {
+router.get('/login', function(req, res) {
   console.log('Na cb do GET login...')
   console.log(req.sessionID)
   res.render('login-form')
 });
 
-router.get('/logout', function (req, res) {
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+
+  //console.log('Na cb do POST login...')
+  console.log('Do form: ' +JSON.stringify(req.body))
+  console.log('Do passport: ' +JSON.stringify(req.user))
+  res.redirect('/protegida')
+})
+  
+router.get('/logout', function(req, res){
   req.logout();
   req.session.destroy(function (err) {
     if (!err) {
-      res.redirect('/');
+        res.redirect('/');
     } else {
-      console.log('Destroy session error: ', err)
+        console.log('Destroy session error: ', err)
     }
   });
-})
-
-router.post('/login', passport.authenticate('local'), function (req, res) {
-  console.log('Na cb do POST login...')
-  console.log('Do form: ' + JSON.stringify(req.body))
-  console.log('Do passport: ' + JSON.stringify(req.user))
-  res.redirect('/protegida')
 });
-
-router.get('/administrador', function (req, res) {
-  if (req.isAuthenticated() && req.user.role == "administrador") {
-    res.render('administrador', { utilizador: req.user });
+ 
+router.get('/administrador', function(req, res) {
+  if(req.isAuthenticated() && req.user.role=="administrador"){
+    res.render('administrador', {utilizador: req.user});
   }
-  else {
+  else{
     res.send("Nao tens permissoes para aceder a esta pagina")
   }
 });
 
-router.get('/consumidor', function (req, res) {
-  if (req.isAuthenticated() && req.user.role == "consumidor") {
-    res.render('consumidor', { utilizador: req.user });
+router.get('/consumidor', function(req, res) {
+  if(req.isAuthenticated() && req.user.role=="consumidor"){
+    res.render('consumidor', {utilizador: req.user});
   }
-  else {
+  else{
     res.send("Nao tens permissoes para aceder a esta pagina")
   }
 });
 
-router.get('/files', function (req, res) {
-  if (req.isAuthenticated() && (req.user.role == "consumidor" || req.user.role == "produtor" || req.user.role == "administrador")) {
-    var d = new Date().toISOString().substr(0, 16)
-    var files = jsonfile.readFileSync('./public/dbFiles.json')
-    res.render('fileList', { utilizador: req.user, files, d });
+router.get('/produtor', function(req, res) {
+  if(req.isAuthenticated() && req.user.role=="produtor"){
+    res.render('produtor', {utilizador: req.user});
   }
-  else {
+  else{
     res.send("Nao tens permissoes para aceder a esta pagina")
   }
 });
+//#endregion
 
-router.get('/files/download/:fname', (req, res) => {
-  if (req.isAuthenticated() && (req.user.role == "consumidor" || req.user.role == "produtor" || req.user.role == "administrador")) {
-    let reqPath = path.join(__dirname, '../public/fileStore/', req.params.fname)
-    res.download(reqPath)
-  }
-  else {
-    res.send("Nao tens permissoes para aceder a esta pagina")
-  }
-})
 
-router.get('/produtor', function (req, res) {
-  if (req.isAuthenticated() && req.user.role == "produtor") {
-    res.render('produtor', { utilizador: req.user });
-  }
-  else {
-    res.send("Nao tens permissoes para aceder a esta pagina")
-  }
+//#region Registo
+router.get('/registar', function(req, res) {
+  res.render('registar')
 });
 
-router.get('/files/upload', function (req, res) {
-  if (req.isAuthenticated() && req.user.role == "produtor") {
-    var d = new Date().toISOString().substr(0, 16)
-    res.render('fileForm', { utilizador: req.user, d });
-  }
-  else {
-    res.send("Nao tens permissoes para aceder a esta pagina")
-  }
+router.post('/registar', function(req, res) {
+  var user=req.body
+  User.insert(user)
+    .then(data => res.render('login-form'))
+    .catch(err => res.render('error', {error: err}))
 });
 
-router.post('/files', upload.array('myFile'), function (req, res) {
+//#endregion
 
-  if (req.isAuthenticated() && req.user.role == "produtor") {
-    var i;
-    for (i = 0; i < req.files.length; i++) {
-      let newPath = path.join(__dirname, '../public/fileStore/', req.files[i].originalname)
-      let oldPath = path.join(__dirname, '../', req.files[i].path)
-
-      fs.rename(oldPath, newPath, function (err) {
-        if (err) throw err
-      })
-      var files = jsonfile.readFileSync('./public/dbFiles.json')
-      var d = new Date().toISOString().substr(0, 16)
-
-      if (req.files.length > 0) {
-        var descri = req.body.desc[i]
-      } else {
-        var descri = req.body.desc
-      }
-
-      files.push(
-        {
-          date: d,
-          name: req.files[i].originalname,
-          size: req.files[i].size,
-          mimetype: req.files[i].mimetype,
-          desc: descri
-        }
-      )
-
-      jsonfile.writeFileSync('./public/dbFiles.json', files)
-
-    }
-    res.redirect('/users/produtor')
-  }
-  else {
-    res.send("Nao tens permissoes para aceder a esta pagina")
-  }
-
-})
 
 module.exports = router;
 
